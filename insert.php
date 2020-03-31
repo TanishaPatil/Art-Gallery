9ee34b24df27d6889dc15d3fa6519f0d87c20593
TanishaPatil <53510429+TanishaPatil@users.noreply.github.com>
2020-03-31 12:14:13 +0530

    The Cart has been updated.

---
+++ insert.php
@@ -0,0 +1,62 @@
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+$gender = $_POST['gender'];
+$email = $_POST['email'];
+$phoneCode = $_POST['phoneCode'];
+$phone = $_POST['phone'];
+if (!empty($username) || !empty($password) || !empty($gender) || !empty($email) || !empty($phoneCode) || !empty($phone)) {
+ $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbname = "form";
+    //create connection
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+    if (mysqli_connect_error()) {
+     die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+    } else {
+     $SELECT = "SELECT email From register Where email = ? Limit 1";
+     $INSERT = "INSERT Into register (username, password, gender, email, phoneCode, phone) values(?, ?, ?, ?, ?, ?)";
+     //Prepare statement
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $stmt->bind_result($email);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+     if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("ssssii", $username, $password, $gender, $email, $phoneCode, $phone);
+      $stmt->execute();
+      echo "New record inserted sucessfully";
+     } else {
+      echo "Someone already register using this email";
+     }
+     $stmt->close();
+     $conn->close();
+    }
+} else {
+ echo "All field are required";
+ die();
+}
+$phoneNumber = $_POST['phone'];
+
+if(!empty($phoneNumber)) // phone number is not empty
+{
+    if(preg_match('/^\d{10}$/',$phoneNumber)) // phone number is valid
+    {
+      $phoneNumber = '0' . $phoneNumber;
+
+      // your other code here
+    }
+    else // phone number is not valid
+    {
+      echo 'Phone number invalid !';
+    }
+}
+else // phone number is empty
+{
+  echo 'You must provid a phone number !';
+}
+?>
---
+++ right_side_cart.php
@@ -0,0 +1,11 @@
+<div class="col-md-3 my_cart_container">
+	<div class="my_cart">
+        <h3>MyCart</h3>
+		<div class="cart_items">
+			<ol>
+				<li><img src="images/Taj%20Mahal.jpeg" alt="Taj Mahal" class="my_cart_items"> <span class="my_cart_items_title">Taj Mahal</span> <span class="my_cart_items_price">Rs. 250</span></li>
+                                <li><img src="images/s1.jpg" alt="s1" class="my_cart_items"> <span class="my_cart_items_title">s1</span> <span class="my_cart_items_price">Rs. 250</span></li>
+            </ol>
+        </div>
+	</div>
+</div>	
